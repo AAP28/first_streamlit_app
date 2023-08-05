84a22d1fddfb6096344c863a35702929e42ddd5f
AAP28 <141360405+AAP28@users.noreply.github.com>
2023-08-05 09:58:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-#New Section to display fruityvice api response
+# New Section to display fruityvice api response
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response)
+# streamlit.text(fruityvice_response.json())
 
 
 # write your own comment -what does the next line do? 
